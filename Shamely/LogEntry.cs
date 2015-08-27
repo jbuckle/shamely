c0cb84249fb8ebde37c1d07f8fba8f9662e3e422
Will Jackson <wjackson64@gmail.com>
2015-08-26 23:35:33 -0400

    Working local database

---
+++ Shamely/LogEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using SQLite;
+
+namespace Shamely
+{
+	public class LogEntry
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get ; set ; }
+
+		//Levels
+		// 0: Good
+		// 1: Bad
+		public int Level { get ; set ; }
+		public DateTime LoggedAt { get ; set ; }
+
+		// Takes an int level (currently the one returned by 
+		// args.Which) and maps it to the appropriate integer 
+		// for the Level field in the LogEntry class.
+		// Currently stupid, but potentially valuable in the future.
+		public static int MapToLevel(int which) {
+			return which;
+		}
+	}
+}
+
--- Shamely/MainActivity.cs
+++ Shamely/MainActivity.cs
@@ -8,15 +8,17 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using SQLite;
+
 namespace Shamely
 {
 	[Activity (Label = "Shamely", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
 		private string listChoice = "";
-		private string[] choices = new string[] { "A healthy choice", 
-			"An okay choice", 
-			"A bad choice"};
+		private string[] choices = new string[] { "A good choice", "A bad choice" };
+		private string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments),
+			"shamely.db");
 
 		private AlertDialog addFoodDialog = null;
 		private AlertDialog confirmFoodDialog = null;
@@ -25,8 +27,14 @@ namespace Shamely
 		{
 			base.OnCreate (bundle);
 
+			using (var db = new SQLiteConnection (dbPath)) { 
+				db.CreateTable<LogEntry> ();
+			}
+
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
+
+			// Create our database if it does not already exist
 
 			// Get our button from the layout resource,
 			// and attach an event to it
@@ -36,10 +44,9 @@ namespace Shamely
 			{
 				AlertDialog.Builder addFoodDialogBuilder = new AlertDialog.Builder(this);
 				addFoodDialogBuilder.SetTitle ("I had something to eat and it was:");
-				addFoodDialogBuilder.SetSingleChoiceItems (choices, 0, clickFoodDialogList);
+				addFoodDialogBuilder.SetSingleChoiceItems (choices, -1, clickFoodDialogList);
 
 				addFoodDialog = addFoodDialogBuilder.Create();
-
 				// Show the alert dialog to the user and wait for response.
 				addFoodDialog.Show();
 			};
@@ -47,6 +54,8 @@ namespace Shamely
 
 		private void clickFoodDialogList(object sender, DialogClickEventArgs args){
 			listChoice = choices [args.Which];
+
+			Console.WriteLine ("Selected: {0}", args.Which);
 
 			// Open a confirmation alert 
 			AlertDialog.Builder confirmFoodDialogBuilder = new AlertDialog.Builder(this);
@@ -56,8 +65,19 @@ namespace Shamely
 			listChoice + ".  Do you wish to proceed?");
 
 			// Insert the selection into the database on confirmation
-			confirmFoodDialogBuilder.SetPositiveButton ("Confirm", delegate { 
+			confirmFoodDialogBuilder.SetPositiveButton ("Confirm", delegate {
 				dismissAddFoodDialog ();
+
+				LogEntry newLog = new LogEntry { 
+					LoggedAt = DateTime.Now, 
+					Level = LogEntry.MapToLevel(args.Which) 
+				};
+
+				using (var db = new SQLiteConnection(dbPath)) { 
+					db.Insert (newLog);
+					var count = db.ExecuteScalar<int> ("Select COUNT(*) from LogEntry");
+					Console.WriteLine("There are now {0} Log Entries", count);
+				}
 			});
 
 			// Close all alerts if the user cancels at this point
