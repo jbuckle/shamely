1d006dc6e886b2638638be91a0716f59c1d238f7
Will Jackson <wjackson64@gmail.com>
2015-08-26 23:34:52 -0400

    Add weird asset changes to project

--- Shamely/Resources/Resource.designer.cs
+++ Shamely/Resources/Resource.designer.cs
@@ -90,6 +90,8 @@ namespace Shamely
 			global::com.alliance.calendar.Resource.Styleable.calendarthis_calendar_showonlycurrentmonth = global::Shamely.Resource.Styleable.calendarthis_calendar_showonlycurrentmonth;
 			global::com.alliance.calendar.Resource.Styleable.calendarthis_calendar_showprevmonthsfromcurrentmonth = global::Shamely.Resource.Styleable.calendarthis_calendar_showprevmonthsfromcurrentmonth;
 			global::com.alliance.calendar.Resource.Styleable.calendarthis_calendar_weekstartswith = global::Shamely.Resource.Styleable.calendarthis_calendar_weekstartswith;
+			global::SQLite.Resource.String.ApplicationName = global::Shamely.Resource.String.ApplicationName;
+			global::SQLite.Resource.String.Hello = global::Shamely.Resource.String.Hello;
 		}
 		
 		public partial class Animation
@@ -166,56 +168,56 @@ namespace Shamely
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int black = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int black = 2131099649;
 			
-			// aapt resource value: 0x7f050004
-			public const int daycell_default_bottom = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int daycell_default_bottom = 2131099652;
 			
-			// aapt resource value: 0x7f050003
-			public const int daycell_default_middle = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int daycell_default_middle = 2131099651;
 			
-			// aapt resource value: 0x7f050002
-			public const int daycell_default_top = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int daycell_default_top = 2131099650;
 			
-			// aapt resource value: 0x7f05000a
-			public const int daycell_dim_bottom = 2131034122;
+			// aapt resource value: 0x7f06000a
+			public const int daycell_dim_bottom = 2131099658;
 			
-			// aapt resource value: 0x7f050009
-			public const int daycell_dim_middle = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int daycell_dim_middle = 2131099657;
 			
-			// aapt resource value: 0x7f050008
-			public const int daycell_dim_top = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int daycell_dim_top = 2131099656;
 			
-			// aapt resource value: 0x7f050007
-			public const int daycell_highlight_bottom = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int daycell_highlight_bottom = 2131099655;
 			
-			// aapt resource value: 0x7f050006
-			public const int daycell_highlight_middle = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int daycell_highlight_middle = 2131099654;
 			
-			// aapt resource value: 0x7f050005
-			public const int daycell_highlight_top = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int daycell_highlight_top = 2131099653;
 			
-			// aapt resource value: 0x7f05000d
-			public const int daycell_selected_bottom = 2131034125;
+			// aapt resource value: 0x7f06000d
+			public const int daycell_selected_bottom = 2131099661;
 			
-			// aapt resource value: 0x7f05000c
-			public const int daycell_selected_middle = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int daycell_selected_middle = 2131099660;
 			
-			// aapt resource value: 0x7f05000b
-			public const int daycell_selected_top = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int daycell_selected_top = 2131099659;
 			
-			// aapt resource value: 0x7f050010
-			public const int daynamecell_default_bottom = 2131034128;
+			// aapt resource value: 0x7f060010
+			public const int daynamecell_default_bottom = 2131099664;
 			
-			// aapt resource value: 0x7f05000f
-			public const int daynamecell_default_middle = 2131034127;
+			// aapt resource value: 0x7f06000f
+			public const int daynamecell_default_middle = 2131099663;
 			
-			// aapt resource value: 0x7f05000e
-			public const int daynamecell_default_top = 2131034126;
+			// aapt resource value: 0x7f06000e
+			public const int daynamecell_default_top = 2131099662;
 			
-			// aapt resource value: 0x7f050000
-			public const int white = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int white = 2131099648;
 			
 			static Color()
 			{
@@ -258,47 +260,35 @@ namespace Shamely
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f08000d
-			public const int AddFoodButton = 2131230733;
-			
-			// aapt resource value: 0x7f08000a
-			public const int Alliance_Calendar_Gridview_Days = 2131230730;
-			
 			// aapt resource value: 0x7f080009
-			public const int Alliance_Calendar_GridviewdayNames = 2131230729;
-			
-			// aapt resource value: 0x7f08000b
-			public const int Alliance_Calendar_Num_Day = 2131230731;
-			
-			// aapt resource value: 0x7f080008
-			public const int Alliance_Calendar_btnNext = 2131230728;
+			public const int AddFoodButton = 2131230729;
 			
 			// aapt resource value: 0x7f080006
-			public const int Alliance_Calendar_btnPrev = 2131230726;
-			
-			// aapt resource value: 0x7f080007
-			public const int Alliance_Calendar_txtYearMonth = 2131230727;
-			
-			// aapt resource value: 0x7f080003
-			public const int button1 = 2131230723;
-			
-			// aapt resource value: 0x7f080004
-			public const int button2 = 2131230724;
+			public const int Alliance_Calendar_Gridview_Days = 2131230726;
 			
 			// aapt resource value: 0x7f080005
-			public const int button3 = 2131230725;
+			public const int Alliance_Calendar_GridviewdayNames = 2131230725;
 			
-			// aapt resource value: 0x7f08000c
-			public const int linearLayout1 = 2131230732;
+			// aapt resource value: 0x7f080007
+			public const int Alliance_Calendar_Num_Day = 2131230727;
+			
+			// aapt resource value: 0x7f080004
+			public const int Alliance_Calendar_btnNext = 2131230724;
+			
+			// aapt resource value: 0x7f080002
+			public const int Alliance_Calendar_btnPrev = 2131230722;
+			
+			// aapt resource value: 0x7f080003
+			public const int Alliance_Calendar_txtYearMonth = 2131230723;
+			
+			// aapt resource value: 0x7f080008
+			public const int linearLayout1 = 2131230728;
 			
 			// aapt resource value: 0x7f080000
 			public const int monday = 2131230720;
 			
 			// aapt resource value: 0x7f080001
 			public const int sunday = 2131230721;
-			
-			// aapt resource value: 0x7f080002
-			public const int textView1 = 2131230722;
 			
 			static Id()
 			{
@@ -314,16 +304,13 @@ namespace Shamely
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int AddFood = 2130903040;
+			public const int Alliance_Calendar_Main = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Alliance_Calendar_Main = 2130903041;
+			public const int day_gridcell = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int day_gridcell = 2130903042;
-			
-			// aapt resource value: 0x7f030003
-			public const int Main = 2130903043;
+			public const int Main = 2130903042;
 			
 			static Layout()
 			{
@@ -338,17 +325,17 @@ namespace Shamely
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f060001
-			public const int ApplicationName = 2131099649;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f060000
-			public const int Hello = 2131099648;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
-			// aapt resource value: 0x7f060003
-			public const int app_name = 2131099651;
+			// aapt resource value: 0x7f050003
+			public const int app_name = 2131034115;
 			
-			// aapt resource value: 0x7f060002
-			public const int hello = 2131099650;
+			// aapt resource value: 0x7f050002
+			public const int hello = 2131034114;
 			
 			static String()
 			{
